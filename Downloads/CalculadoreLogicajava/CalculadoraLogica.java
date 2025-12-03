5591c2588a74b0f28c2eb645495f18fecf9cf481
lohapuque <lohoanemarinho2007@gmail.com>
2025-12-02 21:26:47 -0300

    Cálculo do volume

--- Downloads/CalculadoreLogicajava/CalculadoraLogica.java
+++ Downloads/CalculadoreLogicajava/CalculadoraLogica.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class CalculadoraLogica {
-    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        final double PRECO_POR_METRO = 53.39;
+
+        System.out.print("Digite o comprimento do baú (m): ");
+        double comprimento = input.nextDouble();
+
+        System.out.print("Digite a largura do baú (m): ");
+        double largura = input.nextDouble();
+
+        System.out.print("Digite a altura do baú (m): ");
+        double altura = input.nextDouble();
+
+        double volume = comprimento * largura * altura;
+
+
+    }
 }
